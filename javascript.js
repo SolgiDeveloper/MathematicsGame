97059d328dae97aced658d8d6e673acb55e4249b
Mohammad Solgi <mohammadsolgi96@gmail.com>
2020-10-25 22:39:48 +0330

    add action to button

--- javascript.js
+++ javascript.js
@@ -1,1 +1,44 @@
+let startBtn = document.getElementById("startReset");
+let score = document.getElementById("scoreValue");
+let timer = document.getElementById("timeRemaining");
+let gameOver = document.getElementById("gameOver");
+let timerValue = document.getElementById("timeRemainingValue");
+let playerScore;
+let playing = false;
+startBtn.onclick = function () {
+  if (playing) {
+    location.reload();
+    startBtn.innerHTML = "Start Game";
+    playing = false;
+  } else {
+    score.innerHTML = 0;
+    playerScore = 0;
+    timer.style.display = 'inline';
+    start();
+  }
+};
+
+function start() {
+  let t = 59;
+  let starting = setInterval(function () {
+    timerValue.innerHTML = t;
+
+    if (t == 0) {
+      clearInterval(starting);
+      gameOver.innerHTML
+        = "game over!"
+        + "<br>"
+        + "your score is "
+        + playerScore;
+      gameOver.style.display = 'inline';
+      startBtn.innerHTML = "Reset Game";
+      playing = true;
+    }
+    t--;
+  }, 1000);
+
+
+
+
+}
 
